8badbf57bcf362b21296fdfb262a54e636eb4753
Mikasu <59112826+Mikasu-kun@users.noreply.github.com>
2024-02-19 08:18:24 +0100

    Update Filter.jsx

--- part2/phonebook/src/components/Filter.jsx
+++ part2/phonebook/src/components/Filter.jsx
@@ -4,44 +4,5 @@ const Filter = ({ value, onChange }) => (
     </div>
   );
   
-const Notification = ({ message, type }) => {
-  if (message === null) return null;
-
-  return <div className={`notification ${type}`}>{message}</div>;
-};
-
-const Persons = ({ persons, nameFilter, onDelete }) => {
-    const filteredPersons = persons.filter((person) =>
-      person.name.toLowerCase().includes(nameFilter.toLowerCase())
-    );
-  
-    return filteredPersons.map((person) => (
-      <div key={person.name}>
-        {person.name} {person.number}{' '}
-        <button onClick={() => onDelete(person)}>delete</button>
-      </div>
-    ));
-  };
-
-  const PersonForm = ({
-    onSubmit,
-    valueName,
-    onChangeName,
-    valueNumber,
-    onChangeNumber,
-  }) => (
-    <form onSubmit={onSubmit}>
-      <div>
-        name: <input value={valueName} onChange={onChangeName} />
-      </div>
-      <div>
-        number: <input type="tel" value={valueNumber} onChange={onChangeNumber} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  );
-
 
   export default Filter; 
